242eee704269d1cd9044c41f420bb9135d565a52
Sergey <kvazar941@gmail.com>
2022-03-17 11:21:22 +0300

    realised formatter plain

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -170,22 +170,24 @@ def plain2(list_):
     return result
 
 
-def plain(list_):
+def plain(list_, way=''):
     result = ''
-    add_str = 'added with value: '
-    add_str = 'added with value: '
-    rem_str = 'removed'
-    upd_str = 'updated'
+    add_str = 'was added with value: '
+    rem_str = 'was removed'
+    upd_str = 'was updated. From '
     for a in list_:
         a_diff = a['diff']
-        if a_diff['old'] == None:
-            print('Property ', a['key'], 'was added with value: ', a_diff['new'])
-        if a_diff['new'] == None:
-            print('Property ', a['key'], 'was remowed')
-        if a_diff['old'] != a_diff['new'] and a_diff['old'] != None and a_diff['new'] != None:
-            print('Property ', a['key'], 'was updated. From ', a_diff['old'], ' to ', a_diff['new'])
+        if a['type'] == 'dict':
+            plain(a['children'], way + a['key'] + '.')
+        else:
+            if a_diff['old'] == None:
+                result += "Property '{0}' {1}{2}\n".format(way + a['key'], add_str, a_diff['new'])
+            if a_diff['new'] == None:
+                result += "Property '{0}' {1}\n".format(way + a['key'], rem_str) 
+            if a_diff['old'] != a_diff['new'] and a_diff['old'] != None and a_diff['new'] != None:
+                result += "Property '{0}' {1}{2} to {3}\n".format(way + a['key'], upd_str, a_diff['old'], a_diff['new'])
+    print(result)
     return result
-
 
 
 def generate_diff(file1=args.first_file, file2=args.second_file):
