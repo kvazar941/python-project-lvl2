6a67983859c386bf465ff0bf6e9025962d4f5402
Sergey <kvazar941@gmail.com>
2022-03-29 15:52:39 +0300

    immediate

--- gendiff/diffs/diff.py
+++ gendiff/diffs/diff.py
@@ -1,40 +1,10 @@
 """Diff module"""
 
 
-def convert_bool(value):
-    if value == True:
-        return 'true'
-    elif value == False:
-        return 'false'
-    elif value == None:
-        return 'null'
-    return value
-
-
-def convert_to_node(key, value, diff):
-    if type(value) == dict:
-        result = {'key': key,
-                'type': 'dict',
-                'diff': diff,
-                'children': [value]
-                }
-    elif type(value) == list:
-        result = {'key': key,
-                'type': 'dict',
-                'diff': diff,
-                'children': value
-                }
-    else:
-        result = {'key': key,
-                'type': 'elem',
-                'diff': diff,
-                #'value': value
-                }
-    return result
-
-
 def dict_list2(dict_):
     result = []
+    if type(dict_) != dict:
+        return dict_
     for z in dict_:
         if type(dict_[z]) == dict:
             result.append({'key': z, 'children': dict_list2(dict_[z])})
@@ -43,73 +13,38 @@ def dict_list2(dict_):
     return result
 
 
-def func4(elem, key, value_one, value_two):
-    diff = {'old': value_one, 'new': value_two}
-    if type(elem) == dict:
-        return convert_to_node(key, convert_bool(dict_list2(elem)), diff)
-    else:
-        return convert_to_node(key, convert_bool(elem), diff)
-
-
-def create_tree(dict_):
-    result = {}
-    for a in dict_:
-        result[a] = {'diff': {'old': dict_[a]}}
-    return result
-
-
-def create_tree2(dict_):
-    result = {}
-    for a in dict_:
-        result[a] = {'diff': {'new': dict_[a]}}
-    return result
-
-
 def diff_of_list(dict_a, dict_b):
-    key_set = sorted(dict_a.keys() | dict_b.keys())
     result = []
     a_set = dict_a.keys() - dict_b.keys()
     b_set = dict_b.keys() - dict_a.keys()
     ch_set = dict_a.keys() & dict_b.keys()
     
     for a in a_set:
-        #if type(dict_a[a]) == dict:
-        #    result.append({'key': a, 'children': dict_list2(dict_a[a])})
-        #else:
-        result.append({'key': a, 'diff': {'old': dict_a[a]}})
+        if type(dict_a[a]) == dict:
+            result.append({'key': a, 'value': dict_list2(dict_a[a])})
+        else:
+            result.append({'key': a, 'diff': {'old': dict_a[a]}}) 
     for a in b_set:
-        #if type(dict_b[a]) == dict:
-        #result.append({'key': a, 'children': dict_list2(dict_b[a])})
-        #else:
-        result.append({'key': a, 'diff': {'new': dict_b[a]}})
+        if type(dict_b[a]) == dict:
+            result.append({'key': a, 'value': dict_list2(dict_b[a])})
+        else:
+            result.append({'key': a, 'diff': {'new': dict_b[a]}})
     for a in ch_set:
         if type(dict_a[a]) == dict and type(dict_b[a]) == dict:
             result.append({'key': a, 'children': diff_of_list(dict_a[a], dict_b[a])})
         else:
-            result.append({'key': a,
-                'diff': {'old': dict_a[a], 'new': dict_b[a]}
-                })
+            #if type(dict_a[a]) == dict:
+            #    res_a = {'key': a, 'value': dict_list2(dict_a[a])}
+            #else:
+            #    res_a = {'key': a, 'diff': {'old': dict_a[a]}}
+            #if type(dict_b[a]) == dict:
+            #    res_b = {'key': a, 'value': dict_list2(dict_b[a])}
+            #else:
+            #    res_b = {'key': a, 'diff': {'new': dict_b[a]}}
+            
 
-
-        '''
-        if a in dict_a and a in dict_b:
-            argum = (a, convert_bool(dict_a[a]), convert_bool(dict_b[a]))
-            if type(dict_a[a]) == dict and type(dict_b[a]) == dict:
-                result.append(func4(diff_of_list(dict_a[a], dict_b[a]), *argum))
-            else:
-                #if dict_a[a] != dict_b[a]:
-                #    result.append(func4(dict_a[a], *argum))
-                result.append(func4(dict_a[a], *argum))
-        if a in dict_a and a not in dict_b:
-            result.append(func4(dict_a[a], a, convert_bool(dict_a[a]), None))
-        if a not in dict_a and a in dict_b:
-            result.append(func4(dict_b[a], a, None, convert_bool(dict_b[a])))
-        '''
-    x = create_tree(dict_a)
-    y = create_tree2(dict_b)
-
-    #print(x)
-    #print(y)
+            result.append({'key': a, 'diff': {'old': dict_list2(dict_a[a]), 'new': dict_list2(dict_b[a])}})
+            #result.append({'key': a, 'diff': {'old': dict_a[a], 'new': dict_b[a]}})
 
     #print(result)
     return result
--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -11,56 +11,65 @@ def convert_bool(value):
     return value
 
 
-def func1(a, count):
+def func_add(a, count):
     diff_ = a['diff']
     return "{0}{1}: {2}\n".format('    '*count + '  + ', a['key'], convert_bool(diff_['new']))
 
 
-def func2(a, count):
+def func_add_dict(dict_, count):
+    result = ''
+    if type(dict_) == list:
+        result += "{0}{1}: {2}\n".format('    '*count + '    ', dict_['key'], dict_)
+    else:
+        result += "{0}{1}: {2}\n".format('    '*count + '    ', dict_['key'], dict_)
+    return result
+        
+
+
+def func_rem(a, count):
     diff_ = a['diff']
     return "{0}{1}: {2}\n".format('    '*count + '  - ', a['key'], convert_bool(diff_['old']))
 
 
-def func3(a, count):
+def func_ch(a, count):
     diff_ = a['diff']
     return "{0}{1}: {2}\n".format('    '*count + '    ', a['key'], convert_bool(diff_['new']))
 
 
-def formatter(list_, count=0):
+def func1(list_, count):
     list_sorted = sorted(list_, key = lambda x: x['key'])
-    print(list_sorted)
     result = '{\n'
     for a in list_sorted:
         if 'diff' in a:
             diff_ = a['diff']
             if 'new' in diff_ and 'old' not in diff_:
-                if type(diff_['new']) == dict:
-                    result += "{0}{1}: {2}\n".format('    '*count + '  7 ', a['key'], convert_bool(diff_['new']))
-                else:
-                    result += func1(a, count)
-            elif 'new' not in diff_ and 'old' in diff_:
-                result += func2(a, count)
-            elif diff_['old'] == diff_['new']:
-                result += func3(a, count)
-            else:
-                result += func2(a, count)
-                result += func1(a, count)
-        else:
-            result += "{0}{1}: {2}\n".format('    '*count + '    ', a['key'], formatter(a['children'], count + 1))
-        '''
-        else:
-            if diff_old != diff_new:
-                if type(diff_['old']) == dict and type(diff_['new']) != dict:
-                    diff_result = '  - '
-                elif type(diff_['old']) != dict and type(diff_['new']) == dict:
-                    diff_result = '  + '
-                elif diff_['old'] == None:
-                    diff_result = '  + '
-                elif diff_['new'] == None:
-                    diff_result = '  - '
-            result += "{0}{1}: {2}".format('    '*count + diff_result, a['key'], formatter(a['children'], count + 1))
-        '''
+                result += "{0}{1}: {2}\n".format('    '*count + '    ', a['key'], convert_bool(diff_['new']))
+        elif 'children' in a:
+            result += "{0}{1}: {2}\n".format('    '*count + '    ', a['key'], func1(a['children'], count + 1))
+
     result += '    '*count + '}'
-    result += '\n'
+    return result
+
+
+def formatter(list_, count=0):
+    list_sorted = sorted(list_, key = lambda x: x['key'])
+    result = '{\n'
+    for a in list_sorted:
+        if 'diff' in a:
+            diff_ = a['diff']
+            if 'new' in diff_ and 'old' not in diff_:
+                result += func_add(a, count)
+            elif 'new' not in diff_ and 'old' in diff_:
+                result += func_rem(a, count)
+            elif diff_['old'] == diff_['new']:
+                result += func_ch(a, count)
+            else:
+                result += func_rem(a, count)
+                result += func_add(a, count)
+        elif 'children' in a:
+            result += "{0}{1}: {2}\n".format('    '*count + '    ', a['key'], formatter(a['children'], count + 1))
+        else:
+            result += "{0}{1}: {2}\n".format('    '*count + '  * ', a['key'], func1(a['value'], count + 1))
+    result += '    '*count + '}'
     return result
 
