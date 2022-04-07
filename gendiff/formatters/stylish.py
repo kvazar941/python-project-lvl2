e3d3c086b933c1f2bca6bc76ace72571ed512ab6
Sergey <kvazar941@gmail.com>
2022-04-07 16:56:58 +0300

    refactoring stylish.py

--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -1,46 +1,32 @@
 """Stylish module"""
-import gendiff.formatters.convert_bool
+from gendiff.formatters.convert_bool import convert
 
 
-def convert_bool(value):
-    if value == True:
-        return 'true'
-    elif value == False:
-        return 'false'
-    elif value == None:
-        return 'null'
-    return value
+DEFOLT_INDENT = '    '
+INDENTS = {'defolt': '    ', 'new': '  + ', 'old': '  - '}
+
+
+def add_str(count, indent, key, elem):
+    return "{0}{1}: {2}\n".format(DEFOLT_INDENT*count + indent, key, convert(elem))
 
 
 def format_dict(dict_, count):
-    if type(dict_) != dict:
+    if not isinstance(dict_, dict):
         return dict_
     result = '{\n'
     for a in dict_:
-        result += "{0}{1}: {2}\n".format('    '*count + '    ', a, format_dict(dict_[a], count + 1))
-    result += '    '*count + '}'
+        result += add_str(count, DEFOLT_INDENT, a, format_dict(dict_[a], count + 1))
+    result += DEFOLT_INDENT*count + '}'
     return result
         
 
-def func_add(a, count):
-    diff_ = a['diff']
-    if type(diff_['new']) == dict:
-        return "{0}{1}: {2}\n".format('    '*count + '  + ', a['key'], convert_bool(format_dict(diff_['new'], count + 1)))
-    return "{0}{1}: {2}\n".format('    '*count + '  + ', a['key'], convert_bool(diff_['new']))
-
-
-def func_rem(a, count):
-    diff_ = a['diff']
-    if type(diff_['old']) == dict:
-        return "{0}{1}: {2}\n".format('    '*count + '  - ', a['key'], convert_bool(format_dict(diff_['old'], count + 1)))
-    return "{0}{1}: {2}\n".format('    '*count + '  - ', a['key'], convert_bool(diff_['old']))
-
-
-def func_ch(a, count):
-    diff_ = a['diff']
-    if type(diff_['new']) == dict:
-        return "{0}{1}: {2}\n".format('    '*count + '    ', a['key'], convert_bool(format_dict(diff_['new'], count + 1)))
-    return "{0}{1}: {2}\n".format('    '*count + '    ', a['key'], convert_bool(diff_['new']))
+def func_add(dict_, count, key, indent=INDENTS['defolt']):
+    diff_ = dict_['diff']
+    if isinstance(diff_[key], dict):
+        elem = format_dict(diff_[key], count + 1)
+    else:
+        elem = diff_[key]
+    return add_str(count, indent, dict_['key'], elem)
 
 
 def formatter(list_, count=0):
@@ -49,17 +35,20 @@ def formatter(list_, count=0):
     for a in list_sorted:
         if 'diff' in a:
             diff_ = a['diff']
-            if 'new' in diff_ and 'old' not in diff_:
-                result += func_add(a, count)
-            elif 'new' not in diff_ and 'old' in diff_:
-                result += func_rem(a, count)
+            if 'old' not in diff_:
+                result += func_add(a, count, 'new', '  + ')
+            elif 'new' not in diff_:
+                result += func_add(a, count, 'old', '  - ')
             elif diff_['old'] == diff_['new']:
-                result += func_ch(a, count)
+                result += func_add(a, count, 'new')
             else:
-                result += func_rem(a, count)
-                result += func_add(a, count)
+                result += func_add(a, count, 'old', '  - ')
+                result += func_add(a, count, 'new', '  + ')
         else:
-            result += "{0}{1}: {2}".format('    '*count + '    ', a['key'], formatter(a['children'], count + 1))
-    result += '    '*count + '}\n'
+            result += add_str(count, DEFOLT_INDENT, a['key'], formatter(a['children'], count + 1))
+    if count == 0:
+        result += DEFOLT_INDENT*count + '}\n'
+    else:
+        result += DEFOLT_INDENT*count + '}'
     return result
 
