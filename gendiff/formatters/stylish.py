dd28da035abf34d6a72dc4463857d7e42f5db60f
Sergey <kvazar941@gmail.com>
2022-04-12 01:38:47 +0300

    refactoring stylish.py

--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -1,21 +1,11 @@
 """Stylish module"""
 from gendiff.formatters.convert_bool import convert
-from functools import reduce
 
 
 DEFOLT_INDENT = '    '
 
 
 def get_indent(key):
-    """
-    The function .
-
-    Args:
-        key: str
-
-    Returns:
-        str
-    """
     if key == 'old':
         indent = '  - '
     if key == 'new':
@@ -34,49 +24,6 @@ def convert_dict(dict_, initial_indent):
     return [new_format(indent, key, convert_dict(dict_[key], indent)) for key in dict_]
 
 
-def get_diff_key(diff, key, indent):
-    indent_result = indent + DEFOLT_INDENT
-    result = []
-    for key1 in diff:
-        val = diff[key1]
-        value = convert_dict(val, indent_result) if type(val) == dict else val
-        result.append(new_format(indent + get_indent(key1), key, value))
-    return result
-
-
-def get_diff_dict(dict_, count):
-    result = []
-    indent = DEFOLT_INDENT*count
-    key = dict_['key']
-    if 'diff' in dict_:
-        diff = dict_['diff']
-        if diff.get('old') != diff.get('new'):
-            #x = 
-            result.extend(get_diff_key(diff, key, indent))
-        else:
-            result.append(new_format(indent + DEFOLT_INDENT, key, diff['new']))
-    else:
-        value = convert_format(dict_['children'], count + 1)
-        result.append(new_format(indent + DEFOLT_INDENT, key, value))
-    return result
-
-
-def get_diff_dict2(dict_, count):
-    result = []
-    indent = DEFOLT_INDENT*count
-    key = dict_['key']
-    if 'diff' in dict_:
-        diff = dict_['diff']
-        if diff.get('old') != diff.get('new'):
-            result.extend(get_diff_key(diff, key, indent))
-        else:
-            result.append(new_format(indent + DEFOLT_INDENT, key, diff['new']))
-    else:
-        value = convert_format(dict_['children'], count + 1)
-        result.append(new_format(indent + DEFOLT_INDENT, key, value))
-    return result
-
-
 def convert_format(list_, count_recursion=0):
     """
     The function converts the format {'key': key} to {}.
@@ -87,32 +34,36 @@ def convert_format(list_, count_recursion=0):
     Returns:
         int
     """
-    list_sorted = sorted(list_, key = lambda x: x['key'])
     result = []
-    for dict_ in list_sorted:
-        result.extend(get_diff_dict(dict_, count_recursion))
-
-    result2 = []
 
     list_diff = list(filter(lambda x: 'diff' in x, list_))
+    list_children = list(filter(lambda x: 'children' in x, list_))
+
     indent = DEFOLT_INDENT*count_recursion
+
+    #list_diff_no = list(filter(lambda x: x.get('old') != x.get('new'), list_diff))
+    #list_diff_yes = list(filter(lambda x: x.get('old') == x.get('new'), list_diff))
+    #for z in list_diff_yes:
+        #diff_z = z['diff']
+        #print(list_diff_yes)
+        #result.append(new_format(indent + DEFOLT_INDENT, z['key'], diff_z['new']))
     for a in list_diff:
         diff = a['diff']
         key = a['key']
-        if diff.get('old') != diff.get('new'): 
-            result2.extend(get_diff_key(diff, key, indent))
+        if diff.get('old') != diff.get('new'):
+            for key1 in diff:
+                val = diff[key1]
+                value = convert_dict(val, indent + DEFOLT_INDENT) if type(val) == dict else val
+                result.append(new_format(indent + get_indent(key1), key, value))
         else:
-            result2.append(new_format(indent + DEFOLT_INDENT, key, diff['new']))
-
-    
-    list_children = list(filter(lambda x: 'children' in x, list_))
-
+            result.append(new_format(indent + DEFOLT_INDENT, key, diff['new']))
     for b in list_children:
         key = b['key']
         value = convert_format(b['children'], count_recursion + 1)
-        result2.append(new_format(indent + DEFOLT_INDENT, key, value))
-    result3 = sorted(result2, key = lambda x: x['key'])
-    return result3 # [{}, {}, {}]
+        result.append(new_format(indent + DEFOLT_INDENT, key, value))
+
+    result2 = sorted(result, key = lambda x: x['key'])
+    return result2 # [{}, {}, {}]
 
 
 def convert_to_str(indent, key, value, count):
