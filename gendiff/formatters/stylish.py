b5150fde61ce9fe97b4e5619dce68205d7013918
Sergey <kvazar941@gmail.com>
2022-05-05 16:44:02 +0300

    For checking

--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -17,7 +17,7 @@ METODS = {ADDED: get_new,
           NOT_MODIFIED: get_new}
 
 
-def formate_string(arg_one, arg_two, arg_three):
+def get_string(arg_one, arg_two, arg_three):
     return '{0}{1}: {2}'.format(arg_one, arg_two, arg_three)
 
 
@@ -25,25 +25,31 @@ def connect_string(*args):
     return ''.join(args)
 
 
-def add_curly_braces(list_data, indent):
-    list_result = ['{']
-    list_result.extend(list_data)
-    list_result.append(connect_string(indent, '}'))
-    return list_result
-
-
 def add_indent(string):
     return connect_string(DEFAULT_INDENT + string)
 
 
-def formate_content(content_key, indent=''):
-    if not isinstance(content_key, dict):
-        return str(convert(content_key))
-    list_str = []
-    for key in content_key:
-        current_value = formate_content(content_key[key], add_indent(indent))
-        list_str.append(formate_string(add_indent(indent), key, current_value))
-    return '\n'.join(add_curly_braces(list_str, indent))
+def formate_content(content_key):
+    """
+    Format the key value.
+
+    Args:
+        content_key: Any
+
+    Returns:
+        str
+    """
+    def inner(data_key, indent):
+        if not isinstance(data_key, dict):
+            return str(convert(data_key))
+        list_str = ['{']
+        for key in data_key:
+            current_value = inner(data_key[key], add_indent(indent))
+            string = get_string(add_indent(indent), key, current_value)
+            list_str.append(string)
+        list_str.append(connect_string(indent, '}'))
+        return '\n'.join(list_str)
+    return inner(content_key, '')
 
 
 def make_string_key(indent, key):
@@ -76,7 +82,7 @@ def make_full_string(node, indent, node_type):
             list_result.append(add_indent(indent) + string)
         else:
             list_result.append(string)
-    return formate_string(indent, key, '\n'.join(list_result))
+    return get_string(indent, key, '\n'.join(list_result))
 
 
 def make_string_node(node, indent):
@@ -109,12 +115,13 @@ def formatter(list_dict, indent=''):
         str
     """
     list_dict.sort(key=lambda node: node['key'])
-    result_list = []
+    result_list = ['{']
     for node in list_dict:
         if node[TYPE] == NESTED:
             key = make_string_key(INDENTS[NESTED], get_key(node))
             key_value = formatter(get_children(node), add_indent(indent))
-            result_list.append(formate_string(indent, key, key_value))
+            result_list.append(get_string(indent, key, key_value))
         else:
             result_list.extend(make_string_node(node, indent))
-    return '\n'.join(add_curly_braces(result_list, indent))
+    result_list.append(connect_string(indent, '}'))
+    return '\n'.join(result_list)
