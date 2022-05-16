37a54834b38cda76fc8cedf455a4b0473d2b3018
Sergey <kvazar941@gmail.com>
2022-05-16 16:50:23 +0300

    refactoring stylish.py

--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -27,6 +27,12 @@ def add_indent(string):
     return ''.join([DEFAULT_INDENT, string])
 
 
+def retreat(current_indent, list_):
+    list_result = [current_indent + list_[0]]
+    list_result.extend(list(map(add_indent, list_[1:])))
+    return list_result
+
+
 def get_string(arg_one, arg_two):
     return '{0}: {1}'.format(arg_one, arg_two)
 
@@ -42,24 +48,27 @@ def make_flat(list_):
     return [element for sublist in list_ for element in sublist]
 
 
-def formate_content(content_key):
+def formate(key, key_value):
     """
     Format the key value.
 
     Args:
-        content_key: Any
+        key: str
+        key_value: Any
 
     Returns:
         list
     """
-    def inner(data_key, indent):
-        if not isinstance(data_key, dict):
-            return str(convert(data_key))
-        child = [inner(data_key[key], add_indent(indent)) for key in data_key]
-        step_one = map(get_string, data_key.keys(), child)
-        step_two = list(map(add_indent, step_one))
-        return '\n'.join(add_braces(step_two, indent))
-    return inner(content_key, '').split('\n')
+    def inner(str_one, str_two, indent=''):
+        if not isinstance(str_two, dict):
+            return get_string(f'{indent}{str_one}', convert(str_two))
+        new_indent = add_indent(indent)
+        children = [inner(key, str_two[key], new_indent) for key in str_two]
+        list_result = [get_string(f'{indent}{str_one}', '{')]
+        list_result.extend(children)
+        list_result.append(''.join([indent, '}']))
+        return '\n'.join(list_result)
+    return inner(key, key_value).split('\n')
 
 
 def make_full_string(node, node_type):
@@ -73,11 +82,8 @@ def make_full_string(node, node_type):
     Returns:
         list
     """
-    content_key = formate_content(node[METODS[node_type]])
-    first_string = get_string(node[KEY], content_key[0])
-    list_result = [''.join([INDENTS[node_type], first_string])]
-    list_result.extend(list(map(add_indent, content_key[1:])))
-    return list_result
+    full_string = formate(node[KEY], node[METODS[node_type]])
+    return retreat(INDENTS[node_type], full_string)
 
 
 def make_string_node(node, indent):
@@ -99,17 +105,17 @@ def make_string_node(node, indent):
     return make_flat(list_str)
 
 
-def formatter(list_dict, indent=''):
+def formatter(list_node, indent=''):
     """
     Create a formatted string for console output.
 
     Args:
-        list_dict: list
+        list_node: list
         indent: str
 
     Returns:
         str
     """
-    list_dict.sort(key=lambda node: node[KEY])
-    list_string = [make_string_node(node, indent) for node in list_dict]
+    list_node.sort(key=lambda node: node[KEY])
+    list_string = [make_string_node(node, indent) for node in list_node]
     return '\n'.join(add_braces(make_flat(list_string), indent))
